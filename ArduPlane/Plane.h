3d13ea788b114fe2bc5c1c0aca6fdabc251f2ed4
VladimirAndreev1985 <134856607+VladimirAndreev1985@users.noreply.github.com>
2025-02-24 18:12:58 +0300

    Update Plane.h

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -1299,6 +1299,7 @@ private:
 public:
     void failsafe_check(void);
     void check_attitude_failsafe();  // Добавляем объявление функции
+    void activate_parachute();  // Добавляем объявление
     bool is_landing() const override;
     bool is_taking_off() const override;
 #if AP_SCRIPTING_ENABLED || AP_EXTERNAL_CONTROL_ENABLED
