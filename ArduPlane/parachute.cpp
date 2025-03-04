5510d7cf33833b7165e2fbaead7e4dfb66eb34f7
VladimirAndreev1985 <134856607+VladimirAndreev1985@users.noreply.github.com>
2025-03-04 23:57:40 +0300

    Update parachute.cpp

--- ArduPlane/parachute.cpp
+++ ArduPlane/parachute.cpp
@@ -40,10 +40,8 @@ void Plane::parachute_release()
     // Открываем крышку подушки (Servo3 = канал 2) на 5 секунд
     SRV_Channels::set_output_pwm_chan_timeout(2, 2000, 5000);
 
-    // Включаем насос подушки (Servo4 = канал 3) через 1 секунду на 5 минут
-    AP::scheduler->schedule_delayed(1000, []() {
-        SRV_Channels::set_output_pwm_chan_timeout(3, 1600, 300000); // 5 минут
-    });
+    // Включаем насос подушки (Servo4 = канал 3) на 5 минут
+    SRV_Channels::set_output_pwm_chan_timeout(3, 1600, 300000);
 }
 /*
   parachute_manual_release - trigger the release of the parachute,
