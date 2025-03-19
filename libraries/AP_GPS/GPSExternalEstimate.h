c7544c46af2434870684145592dbe421893fd40c
VladimirAndreev1985 <134856607+VladimirAndreev1985@users.noreply.github.com>
2025-03-19 14:54:55 +0300

    Update GPSExternalEstimate.h

--- libraries/AP_GPS/GPSExternalEstimate.h
+++ libraries/AP_GPS/GPSExternalEstimate.h
@@ -5,11 +5,13 @@
 
 class GPSExternalEstimate : public AP_GPS_Backend {
 public:
-    GPSExternalEstimate(AP_GPS &_gps, AP_GPS::GPS_State &_state);
+    GPSExternalEstimate(AP_GPS &_gps, AP_GPS::Params &_params, AP_GPS::GPS_State &_state);
 
     bool read() override;
 
     void set_position(int32_t lat, int32_t lon, float alt, uint32_t time_ms);
+
+    const char *name() const override { return "ExternalEstimate"; }
 
 private:
     uint32_t _last_update_ms;
