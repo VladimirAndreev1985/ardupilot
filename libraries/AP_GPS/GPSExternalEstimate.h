a85c6496f76095ae6ef8b10eb61218b8c069c826
VladimirAndreev1985 <134856607+VladimirAndreev1985@users.noreply.github.com>
2025-03-19 12:59:13 +0300

    Update GPSExternalEstimate.h

--- libraries/AP_GPS/GPSExternalEstimate.h
+++ libraries/AP_GPS/GPSExternalEstimate.h
@@ -1,21 +1,18 @@
 #pragma once
 
-#include "AP_GPS.h"  // <-- базовый класс GPSDriver
+#include "AP_GPS.h"
+#include "AP_GPS_Backend.h" // <--- добавляем это!
 
 class GPSExternalEstimate : public AP_GPS_Backend {
 public:
-    GPSExternalEstimate();
+    GPSExternalEstimate(AP_GPS &gps, AP_GPS::GPS_State &_state);
 
-    // вызывается периодически внутри AP_GPS, чтобы прочитать новые данные
     int8_t read() override;
-
-    // вызывается при инициализации драйвера GPS
     bool configure() override;
 
-    // Наш метод, чтобы установить данные извне (lat, lon, alt, time_ms)
     void set_position(int32_t lat, int32_t lon, float alt, uint32_t time_ms);
 
 private:
     uint32_t _last_update_ms;
-    Location _loc;  // В ArduPilot Location хранит lat/lng в int32 (1e7 формат)
+    Location _loc;
 };
