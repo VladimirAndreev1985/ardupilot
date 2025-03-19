e908ef7820aa9fa971e9467442070dfb35f91afa
VladimirAndreev1985 <134856607+VladimirAndreev1985@users.noreply.github.com>
2025-03-19 14:55:23 +0300

    Update GPSExternalEstimate.cpp

--- libraries/AP_GPS/GPSExternalEstimate.cpp
+++ libraries/AP_GPS/GPSExternalEstimate.cpp
@@ -1,8 +1,10 @@
 #include "GPSExternalEstimate.h"
 #include <AP_HAL/AP_HAL.h>
 
-GPSExternalEstimate::GPSExternalEstimate(AP_GPS &_gps, AP_GPS::GPS_State &_state) :
-    AP_GPS_Backend(_gps, _state),
+extern const AP_HAL::HAL &hal;
+
+GPSExternalEstimate::GPSExternalEstimate(AP_GPS &_gps, AP_GPS::Params &_params, AP_GPS::GPS_State &_state) :
+    AP_GPS_Backend(_gps, _params, _state, nullptr),  // <-- nullptr, если нет UART
     _last_update_ms(0)
 {
     _loc.lat = 0;
